5e27cf170173d02f802a1cec251030b3c73825a8
fugrusha <andrew.golowko@gmail.com>
2019-12-06 21:47:53 +0200

    Home work 1 Create HealthController and HealthControllerTest

---
+++ src/main/java/com/golovko/backend/controller/HealthController.java
@@ -0,0 +1,10 @@
+package com.golovko.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController{
+    @GetMapping("/health")
+    public void health() {}
+}
