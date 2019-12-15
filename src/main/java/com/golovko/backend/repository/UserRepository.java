bd67d7b6a9a928ec999776092fc26f8b94ba9c82
fugrusha <andrew.golowko@gmail.com>
2019-12-15 16:26:53 +0200

    Home-work-3 Installed PostgreSQL and connected it to the project. Switched on validation of schemas. HSQLDB is defined for test scope only

--- src/main/java/com/golovko/backend/repository/UserRepository.java
+++ src/main/java/com/golovko/backend/repository/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
+
 public interface UserRepository extends CrudRepository<User, UUID> {
 }
--- src/test/java/com/golovko/backend/repository/UserRepositoryTest.java
+++ src/test/java/com/golovko/backend/repository/UserRepositoryTest.java
@@ -5,11 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Sql(statements = "delete from users", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
