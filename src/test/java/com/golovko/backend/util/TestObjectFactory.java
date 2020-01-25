f58015cb7debc24f84642b99218753f41305378d
fugrusha <andrew.golowko@gmail.com>
2020-01-26 01:06:08 +0200

    Added MovieParticipationControllerTest for get ReadDTO and get ReadExtendedDTO

--- src/test/java/com/golovko/backend/util/TestObjectFactory.java
+++ src/test/java/com/golovko/backend/util/TestObjectFactory.java
@@ -2,15 +2,23 @@ package com.golovko.backend.util;
 
 import com.golovko.backend.domain.Gender;
 import com.golovko.backend.domain.Movie;
+import com.golovko.backend.domain.PartType;
 import com.golovko.backend.domain.Person;
+import com.golovko.backend.dto.movie.MovieReadDTO;
+import com.golovko.backend.dto.movieParticipation.MoviePartReadDTO;
+import com.golovko.backend.dto.movieParticipation.MoviePartReadExtendedDTO;
+import com.golovko.backend.dto.person.PersonReadDTO;
 import com.golovko.backend.repository.MovieRepository;
 import com.golovko.backend.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
-@Service
+@Component
 public class TestObjectFactory {
 
     @Autowired
@@ -37,5 +45,55 @@ public class TestObjectFactory {
         person.setGender(Gender.FEMALE);
         person = personRepository.save(person);
         return person;
+    }
+
+    public PersonReadDTO createPersonReadDTO() {
+        PersonReadDTO dto = new PersonReadDTO();
+        dto.setId(UUID.randomUUID());
+        dto.setFirstName("Max");
+        dto.setLastName("Popov");
+        dto.setGender(Gender.MALE);
+        return dto;
+    }
+
+    public MovieReadDTO createMovieReadDTO() {
+        MovieReadDTO readDTO = new MovieReadDTO();
+        readDTO.setId(UUID.randomUUID());
+        readDTO.setMovieTitle("Guess Who");
+        readDTO.setDescription("12345");
+        readDTO.setReleaseDate(LocalDate.parse("1990-12-05"));
+        readDTO.setReleased(false);
+        readDTO.setAverageRating(8.3);
+        return readDTO;
+    }
+
+    public MoviePartReadDTO createMoviePartReadDTO() {
+        MoviePartReadDTO dto = new MoviePartReadDTO();
+        dto.setId(UUID.randomUUID());
+        dto.setPartInfo("Some text");
+        dto.setAverageRating(9.2);
+        dto.setPersonId(UUID.randomUUID());
+        dto.setMovieId(UUID.randomUUID());
+
+        Set<PartType> types = new HashSet<>();
+        types.add(PartType.WRITER);
+        types.add(PartType.COSTUME_DESIGNER);
+        dto.setPartTypes(types);
+        return dto;
+    }
+
+    public MoviePartReadExtendedDTO createMoviePartReadExtendedDTO(PersonReadDTO personDTO, MovieReadDTO movieDTO) {
+        MoviePartReadExtendedDTO dto = new MoviePartReadExtendedDTO();
+        dto.setId(UUID.randomUUID());
+        dto.setPartInfo("Some text");
+        dto.setAverageRating(9.2);
+        dto.setPerson(personDTO);
+        dto.setMovie(movieDTO);
+
+        Set<PartType> types = new HashSet<>();
+        types.add(PartType.WRITER);
+        types.add(PartType.COSTUME_DESIGNER);
+        dto.setPartTypes(types);
+        return dto;
     }
 }
