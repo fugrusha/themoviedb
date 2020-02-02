b3eaba8c7628daf1eaf304b85202cec0e9085728
fugrusha <andrew.golowko@gmail.com>
2020-02-02 17:22:37 +0200

    Home-work-11 Added filters Add ActiveProfile to MovieCastServiceTest

--- src/test/java/com/golovko/backend/service/MovieCastServiceTest.java
+++ src/test/java/com/golovko/backend/service/MovieCastServiceTest.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +24,7 @@ import java.util.UUID;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 @Sql(statements = {"delete from person", "delete from movie", "delete from movie_cast"},
         executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class MovieCastServiceTest {
